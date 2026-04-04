b7291d6a037ceb8e22b7b86520adeed64d341bfa
B0K1NG <dominykaspavlijus@gmail.com>
2026-04-05 02:11:07 +0300

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(cors())
@@ -97,6 +98,6 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on port 3000')
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
